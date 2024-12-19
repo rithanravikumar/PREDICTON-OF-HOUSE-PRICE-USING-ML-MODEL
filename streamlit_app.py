aa5fd7a0e3f9b8a65947a3ecc0a96115cb7ba7fa
rithanravikumar <rithanravikumar@gmail.com>
2024-12-19 17:22:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ model11 = load_model()
 
 # Streamlit App Title
 st.title("Predicting House Prices Based on Past Sales Data")
-st.image("https://github.com/rithanravikumar/PREDICTON-OF-HOUSE-PRICE-USING-ML-MODEL/blob/main/png-transparent-kerala-house.png")
+st.image("https://raw.githubusercontent.com/Hariprasath1911/sale-app/main/1.png")
 st.write("Enter the required details below to predict the house price.")
 
 # Input Fields
